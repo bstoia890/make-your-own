51a4d6fb93a3c8b294d98617eff710f40add745f
bstoia890 <76743955+bstoia890@users.noreply.github.com>
2021-08-02 15:16:45 -0500

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50);
+    text("***Must type in all capitals to work***", 700, 30);
+    text("N F U O I N C T", 700, 150);
+    text("S E A T A A D", 350, 350);
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70);
+    text("Hint? : Uses set of commands to preform a task", 600, 170);
+    text("Hint: Stores data", 350, 370);
+// add code for displaying rest of the names and the hints.
+
+    
+
+}
